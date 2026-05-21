e14be5c38b3f8c1386245667ece9e79b164786ea
anbugz <andreybugz@gmail.com>
2026-05-21 08:12:08 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -6,7 +6,7 @@ from .ai import ask_deepseek
 from .stt import speech_to_text
 from .calc import calculate_customs
 from .security import full_security_scan, is_blocked, unblock_user
-from .tnved import search_tnved, get_radio_list
+from .tnved import search_tnved, is_radio_electronics
 
 __all__ = [
     "get_cbr_rates",
@@ -17,5 +17,5 @@ __all__ = [
     "is_blocked",
     "unblock_user",
     "search_tnved",
-    "get_radio_list",
+    "is_radio_electronics",
 ]
