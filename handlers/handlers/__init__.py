b506663b045ee961d49df9335aef36baacb2fcc6
anbugz <andreybugz@gmail.com>
2026-05-22 13:54:41 +0300

    Delete handlers/handlers directory

--- handlers/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-handlers — регистрация всех хэндлеров aiogram.
-"""
-from handlers import commands, admin, documents, voice, text
-
-__all__ = ["commands", "admin", "documents", "voice", "text"]
